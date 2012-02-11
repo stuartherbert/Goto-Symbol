5a637beaa13dd59dc6ad0d9810857625652eadc8
Crazycooder <crazycooder@gmail.com>
2012-02-11 10:18:32 +0100

    Replaced Symbol::db() by Symbol::_repr__()

--- Goto Symbol.py
+++ Goto Symbol.py
@@ -19,8 +19,8 @@ class Symbol(sublime.Region):
     def __str__(self):
         return "%s[:%s] %s" % (self.file, self.line, self.name)
 
-    def db(self):
-        print self
+    def __repr__(self):
+        return "%s[:%s] %s" % (self.file, self.line, self.name)
 
     def get_line(self):
         return self.view.rowcol(self.begin())[0] + 1
